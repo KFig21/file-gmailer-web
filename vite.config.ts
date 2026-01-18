f59d1a90207c85cfd0e9a81d5a3bff32baf2a199
KevinFigenshu <kfig21@gmail.com>
2026-01-18 16:28:38 -0500

    Fix Vite base path for Heroku

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/file-gmailer-web/', // ← crucial for GitHub Pages
-})
+  // base: '/file-gmailer-web/', // ← crucial for GitHub Pages
+  base: '/', // heroku deployment
+});
